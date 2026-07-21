9d13e178f287d7e9e41f7a873df156149e6dd7d9
wisebear-KOR <dmswodi123@gmail.com>
2026-07-21 11:46:46 +0900

    feat: add emotion dialogues and pet scaling

--- preload.js
+++ preload.js
@@ -5,5 +5,7 @@ contextBridge.exposeInMainWorld("deskPet", {
   onSpeechMode: callback => ipcRenderer.on("pet:speech-mode", (_, mode) => callback(mode)),
   onDragStart: callback => ipcRenderer.on("pet:drag-start", () => callback()),
   onDragEnd: callback => ipcRenderer.on("pet:drag-end", () => callback()),
+  onScale: callback => ipcRenderer.on("pet:scale", (_, scale) => callback(scale)),
   setBubbleHeight: height => ipcRenderer.send("pet:bubble-height", height),
+  setScale: scale => ipcRenderer.send("pet:set-scale", scale),
 });
